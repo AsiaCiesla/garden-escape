0edfdb92a1cf238b858ceec8913d3eeae1354453
Lydia Scadding <lydiascadding@gmail.com>
2021-02-16 20:18:26 +0000

    Tidied up booking form, added validations, and logic on new booking page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,23 @@ class Booking < ApplicationRecord
   belongs_to :garden
 
   validates :start_date_time, :end_date_time, presence: true
+  validate :start_in_future
+  validate :end_after_start
 
   scope :upcoming, -> { where("start_date_time > ?", DateTime.now) }
   scope :past, -> { where("start_date_time < ?", DateTime.now) }
+
+  private
+
+  def end_after_start
+    if end_date_time < start_date_time
+      errors.add(:end_date_time, "must be after the start of the visit.")
+    end
+  end
+
+  def start_in_future
+    if start_date_time < DateTime.now
+      errors.add(:start_date_time, "cannot take place in the past!")
+    end
+  end
 end
