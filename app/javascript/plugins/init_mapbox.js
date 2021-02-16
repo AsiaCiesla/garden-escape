d95158e513657ef766cb58a361f7182be67230b0
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-02-16 20:37:25 +0000

    map moves slower to show all markers and bit extra padding

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, duration: 0 });
+  map.fitBounds(bounds, { padding: 100, duration: 5000 });
 };
 
 const initMapbox = () => {
